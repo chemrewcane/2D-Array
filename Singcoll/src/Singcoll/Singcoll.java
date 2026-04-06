5cf5f7b6b0a171467265a0a14c42018164a8dc81
Maxenne <Maxenne@LAPTOP-K5VQTBOM.bbrouter>
2026-04-06 22:29:08 +0800

    2D Array

---
+++ Singcoll/src/Singcoll/Singcoll.java
@@ -0,0 +1,51 @@
+package Singcoll;
+
+import java.util.Scanner;
+
+public class Singcoll {
+	public static void main(String []args) {
+
+		int numbers [][] =
+			{{120,250,789,7},{58,79,31,87},{8,1,2,3},{99,98,97,96}};
+
+		System.out.println("Print the array in table form: ");
+		for (int x = 0; x<numbers.length;x++) {
+			for (int y = 0; y<numbers[x].length;y++) {	
+				System.out.print(numbers[x][y] + " ");
+			}
+			System.out.println();
+		}
+
+		System.out.println("\nFind the index of number 1: ");
+		for (int x = 0; x<numbers.length;x++) {
+			for (int y = 0; y<numbers[x].length;y++) {	
+				if (numbers[x][y] == 1) {
+					System.out.print("Number 1 is found at indices " + x + " and " + y + "\n");
+					numbers[x][y] = 50;
+				}
+			}
+		}
+
+		System.out.println("\nUpdate number 1 -> 50: ");
+		for (int x = 0; x<numbers.length;x++) {
+			for (int y = 0; y<numbers[x].length;y++) {	
+				System.out.print(numbers[x][y] + " ");
+			}
+			System.out.println(" ");
+		}		
+
+		System.out.println("\nUpdate 2D Array where row 2 is all zero: ");
+		for (int x = 0; x<numbers.length;x++) {
+			for(int y = 0; y<numbers[x].length; y++) {
+				numbers[1][y] = 0;
+			}
+		}
+
+		for(int x = 0; x<numbers.length; x++) {
+			for (int y = 0; y<numbers[x].length; y++) {
+				System.out.print(numbers[x][y] + " ");
+			}
+			System.out.println("");
+		}		
+	}	
+}
